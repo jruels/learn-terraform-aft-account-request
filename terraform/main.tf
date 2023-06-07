fdc8411c8dae0be7722a8d109f0a0bc6c9aa8e22
Jason Smith <jason@smithss.org>
2023-06-06 21:04:17 -0700

    account03

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "aws-account02@innovationinsoftware.com"
-    AccountName               = "Account02 AFT"
+    AccountEmail              = "aws-account03@innovationinsoftware.com"
+    AccountName               = "Account03 AFT"
     ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "aws-account02@innovationinsoftware.com"
-    SSOUserFirstName          = "Account02"
+    SSOUserEmail              = "aws-account03@innovationinsoftware.com"
+    SSOUserFirstName          = "Account03"
     SSOUserLastName           = "AFT"
   }
 
@@ -16,7 +16,7 @@ module "sandbox" {
 
   change_management_parameters = {
     change_requested_by = "Jason Smith"
-    change_reason       = "TF Reusable Code 06/07/2023"
+    change_reason       = "TF Reusable Code 06/07/2033"
   }
 
   custom_fields = {
